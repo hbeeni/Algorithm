9d877f678138ee7104728495901b920450c53097
Hyebeen <122146255+hbeeni@users.noreply.github.com>
2023-06-15 22:08:55 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 200 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        char[] chars = input.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] >= 'a' && chars[i] <= 'z') {
+                chars[i] -= 32;
+            } else {
+                chars[i] += 32;
+            }
+        }
+
+        System.out.println(chars);
+    }
+}
