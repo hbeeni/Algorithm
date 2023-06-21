0e73c084b30dec1d05ef15921e358794c876ad30
Hyebeen <hyebeen.dev@gmail.com>
2023-06-21 10:06:17 +0900

    230621 이진 검색 - Arrays.binarySearch

---
+++ 자료구조와 함께 배우는 알고리즘 입문 (자바 편)/search_algorithm/src/binary/StringBinarySearch.java
@@ -0,0 +1,28 @@
+package binary;
+
+import java.util.Arrays;
+
+public class StringBinarySearch {
+    public static void main(String[] args) {
+        String[] x = {    // 자바에서 사용하는 키워드(알파벳 순)
+                "abstract",   "assert",       "boolean",   "break",      "byte",
+                "case",       "catch",        "char",      "class",      "const",
+                "continue",   "default",      "do",        "double",     "else",
+                "enum",       "extends",      "final",     "finally",    "float",
+                "for",        "goto",         "if",        "implements", "import",
+                "instanceof", "int",          "interface", "long",       "native",
+                "new",        "package",      "private",   "protected",  "public",
+                "return",     "short",        "static",    "strictfp",   "super",
+                "switch",     "synchronized", "this",      "throw",      "throws",
+                "transient",  "try",          "void",      "volatile",   "while"
+        };
+        String key = "int";
+
+        int idx = Arrays.binarySearch(x, key);
+        if (idx < 0) {
+            System.out.println("해당 키워드가 없습니다.");
+        } else {
+            System.out.println("해당 키워드는 x[" + idx + "]에 있습니다.");
+        }
+    }
+}
