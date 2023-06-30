0f6b538510908f0ac060e88030101d51c89f4819
Hyebeen <hyebeen.dev@gmail.com>
2023-06-30 11:15:46 +0900

    B11720 - 숫자의 합

---
+++ 알고리즘 코딩테스트 (자바 편)/datastructure/src/arrayandlist/B11720_s.java
@@ -0,0 +1,20 @@
+package arrayandlist;
+
+import java.util.Scanner;
+
+public class B11720_s {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();
+
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            sum += cNum[i] - '0';
+        }
+
+        System.out.println(sum);
+    }
+}
