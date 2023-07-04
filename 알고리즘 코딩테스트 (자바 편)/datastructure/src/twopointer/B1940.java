5e8925a17a8ed4e4b1adde3ef26dbb60a412f705
Hyebeen <hyebeen.dev@gmail.com>
2023-07-05 08:47:52 +0900

    B1940 - 주몽

---
+++ 알고리즘 코딩테스트 (자바 편)/datastructure/src/twopointer/B1940.java
@@ -0,0 +1,36 @@
+package twopointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class B1940 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        Map<Integer, Integer> list = new HashMap<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            list.put(num, list.getOrDefault(num, 0) + 1);
+        }
+
+        int result = 0;
+
+        for (Integer num : list.keySet()) {
+            if (2 * num != m && list.containsKey(m - num)) {
+                result += list.get(num) * list.get(m - num);
+                list.put(num, 0);
+                list.put(m - num, 0);
+            }
+        }
+
+        System.out.println(result);
+    }
+}
---
+++ 알고리즘 코딩테스트 (자바 편)/datastructure/src/twopointer/B1940_s.java
@@ -0,0 +1,43 @@
+package twopointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B1940_s {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[] a = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+        int count = 0;
+        int s = 0;
+        int e = n - 1;
+
+        while (s < e) {
+            if (a[s] + a[e] == m) {
+                count++;
+                s++;
+                e--;
+            } else if (a[s] + a[e] < m) {
+                s++;
+            } else {
+                e--;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
