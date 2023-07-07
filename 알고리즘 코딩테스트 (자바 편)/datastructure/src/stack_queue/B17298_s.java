84b85fd3329be47d2fa2f76fe810bdd5d8124dce
Hyebeen <hyebeen.dev@gmail.com>
2023-07-08 08:53:10 +0900

    B17298 - 오큰수

---
+++ 알고리즘 코딩테스트 (자바 편)/datastructure/src/stack_queue/B17298_s.java
@@ -0,0 +1,41 @@
+package stack_queue;
+
+import java.io.*;
+import java.util.Stack;
+
+public class B17298_s {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] ans = new int[n];
+
+        String[] str = br.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(str[i]);
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0); //최초 스택 초기화
+
+        for (int i = 1; i < n; i++) {
+            while (!stack.empty() && a[stack.peek()] < a[i]) {
+                ans[stack.pop()] = a[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.empty()) {
+            ans[stack.pop()] = -1;
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < n; i++) {
+            bw.write(ans[i] + " ");
+        }
+
+        bw.flush();
+    }
+}
