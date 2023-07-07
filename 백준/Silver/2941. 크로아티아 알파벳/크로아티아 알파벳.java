c44752511179098411d7e33fe9658248e5eaedc8
Hyebeen <122146255+hbeeni@users.noreply.github.com>
2023-07-07 10:41:11 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,10 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String word = sc.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
         List<String> cro = List.of(
                 "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=");
         int result = 0;
