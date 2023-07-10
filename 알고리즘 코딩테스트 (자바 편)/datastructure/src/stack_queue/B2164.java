93f73fff3bc8649b065998c17a0c68e0556e22fd
hbeeni <hyebeen.dev@gmail.com>
2023-07-10 11:24:21 +0900

    백준 2164번: 카드2

---
+++ 알고리즘 코딩테스트 (자바 편)/datastructure/src/stack_queue/B2164.java
@@ -0,0 +1,26 @@
+//책과 풀이 동일
+
+package stack_queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class B2164 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
