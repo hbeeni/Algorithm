4b4c947a2da500cb9fe6cdf3608145e6d59c7f2d
hbeeni <hyebeen.dev@gmail.com>
2023-07-10 10:34:22 +0900

    프로그래머스 Lv. 1 - 햄버거 만들기

---
+++ workspace/src/programmers_lv1/Hamburger.java
@@ -0,0 +1,39 @@
+package programmers_lv1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hamburger {
+    public int solution(int[] ingredient) {
+        //빵(1) 야채(2) 고기(3)
+        List<Integer> list = new ArrayList<>();
+        int answer = 0;
+
+        for (int i : ingredient) {
+            list.add(i);
+            int size = list.size();
+
+            if (size >= 4) {
+                if (list.get(size - 1) == 1 &&
+                        list.get(size - 2) == 3 &&
+                        list.get(size - 3) == 2 &&
+                        list.get(size - 4) == 1) {
+                    answer++;
+
+                    list.remove(size - 1);
+                    list.remove(size - 2);
+                    list.remove(size - 3);
+                    list.remove(size - 4);
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Hamburger hb = new Hamburger();
+        System.out.println(hb.solution(new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1}));
+        System.out.println(hb.solution(new int[]{1, 3, 2, 1, 2, 1, 3, 1, 2}));
+    }
+}
