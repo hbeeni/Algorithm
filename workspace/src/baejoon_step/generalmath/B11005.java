aa63ccc02475af313517a394f614c32e4ece13c9
hbeeni <hyebeen.dev@gmail.com>
2023-07-10 09:33:21 +0900

    백준 11005번: 진법 변환2

---
+++ workspace/src/baejoon_step/generalmath/B11005.java
@@ -0,0 +1,19 @@
+//https://www.acmicpc.net/source/63191731
+
+package baejoon_step.generalmath;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B11005 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int s = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        System.out.println(Integer.toString(s, n).toUpperCase());
+    }
+}
