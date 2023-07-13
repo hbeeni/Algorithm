89a1708da20e16e4ff05c544b19feaa9b8087ece
hbeeni <hyebeen.dev@gmail.com>
2023-07-13 22:52:26 +0900

    프로그래머스 Lv. 1 - 문자열 나누기

---
+++ workspace/src/programmers_lv1/문자열_나누기.java
@@ -0,0 +1,32 @@
+package programmers_lv1;
+
+public class 문자열_나누기 {
+
+    public static int solution(String s) {
+        int answer = 1;
+
+        char target = s.charAt(0);
+        int count = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (count == 0) {
+                answer++;
+                target = s.charAt(i);
+            }
+
+            if (target == s.charAt(i)) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("banana"));
+        System.out.println(solution("abracadabra"));
+        System.out.println(solution("aaabbaccccabba"));
+    }
+}
