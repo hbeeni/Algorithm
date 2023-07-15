00b9204d849da46f6b83e699f62dea9541ee8730
Hyebeen <122146255+hbeeni@users.noreply.github.com>
2023-07-15 21:27:48 +0900

    [Bronze II] Title: 수 정렬하기, Time: 180 ms, Memory: 16140 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,10 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int n = Integer.parseInt(br.readLine());
         int[] a = new int[n];
 
@@ -12,8 +12,8 @@ public class Main {
             a[i] = Integer.parseInt(br.readLine());
         }
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
                 if (a[j] > a[j + 1]) {
                     int tmp = a[j];
                     a[j] = a[j + 1];
@@ -22,8 +22,10 @@ public class Main {
             }
         }
 
-        for (int num : a) {
-            System.out.println(num);
+        for (int i = 0; i < n; i++) {
+            bw.write(a[i] + "\n");
         }
+
+        bw.flush();
     }
 }
