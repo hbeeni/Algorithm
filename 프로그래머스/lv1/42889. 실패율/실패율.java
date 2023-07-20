3ee9e5fe7786a7453ed6bff1b6e6b6917d32aeb3
Hyebeen <122146255+hbeeni@users.noreply.github.com>
2023-07-20 22:47:00 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.java
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -1,58 +1,43 @@
+//https://geonoo.tistory.com/63
+
 import java.util.*;
 
 class Solution {
-    static class Stage {
-        int stage;
-        float failureRate;
-        
-        public Stage(int stage, float failureRate) {
-            this.stage = stage;
-            this.failureRate = failureRate;
-        }
-    }
-    
     public int[] solution(int N, int[] stages) {
+        Map<Integer, Double> map = new HashMap<>();
         int[] answer = new int[N];
-        int[] fail = new int[N + 1];
-        int[] sum = new int[N + 1];
-        int[] arrival = new int[N + 1];
-        
-        Arrays.sort(stages);
-        
-        for (int stage : stages) {
-            if (stage <= N) {
-                fail[stage]++;
-            }
-        }
-        
+
         for (int i = 1; i <= N; i++) {
-            sum[i] += sum[i - 1] + fail[i];
+            double total = 0;
+            double fail = 0;
+
+            for (int j = 0; j < stages.length; j++) {
+                if (i <= stages[j]) {
+                    total++;
+                }
+                if (i == stages[j]) {
+                    fail++;
+                }
+            }
+
+            map.put(i, total == 0 ? 0 : fail / total);
         }
-        
-        arrival[1] = stages.length;
-        for (int i = 2; i <= N; i++) {
-            arrival[i] = stages.length - sum[i - 1];
-        }
-        
-        Stage[] a = new Stage[N];
+
         for (int i = 0; i < N; i++) {
-            a[i] = new Stage(i + 1, (float) (fail[i + 1]) / arrival[i + 1]);
+            double max = -1;
+            int stage = 0;
+
+            for (Integer key : map.keySet()) {
+                if (max < map.get(key)) {
+                    max = map.get(key);
+                    stage = key;
+                }
+            }
+
+            answer[i] = stage;
+            map.remove(stage);
         }
-        
-        Arrays.sort(a, (s1, s2) -> {
-           if (s2.failureRate - s1.failureRate > 0) {
-               return 1;
-           } else if (s2.failureRate - s1.failureRate < 0) {
-               return -1;
-           } else {
-               return s1.stage - s2.stage;
-           }
-        });
-        
-        for (int i = 0; i < N; i++) {
-            answer[i] = a[i].stage;
-        }
-        
+
         return answer;
     }
 }
