154586ac841e912662a7ef78ca2695b677b20502
Hyebeen <122146255+hbeeni@users.noreply.github.com>
2023-07-20 20:21:20 +0900

    [Silver II] Title: 베르트랑 공준, Time: 224 ms, Memory: 17960 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -3,42 +3,36 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        boolean[] prime = new boolean[123456 * 2 + 1];
+        Arrays.fill(prime, true);
+
+        //에라스토테네스 체 알고리즘
+        prime[0] = false;
+        prime[1] = false;
+
+        for (int i = 2; i <= Math.sqrt(prime.length + 1); i++) {
+            for (int j = i * i; j < prime.length + 1; j += i) {
+                prime[j] = false;
+            }
+        }
+
         while (true) {
             int n = Integer.parseInt(br.readLine());
 
             if (n == 0) {
                 break;
-            } else if (n == 1) {
-                bw.write(1 + "\n");
-                continue;
-            }
-
-            int i = n;
-            if (n % 2 == 0) {
-                i++;
-            } else {
-                i += 2;
             }
 
             int count = 0;
-
-            for (; i <= 2 * n; i += 2) {
-                boolean isPrime = true;
-
-                for (int j = 3; j <= Math.sqrt(i); j += 2) {
-                    if (i % j == 0) {
-                        isPrime = false;
-                        break;
-                    }
-                }
-
-                if (isPrime) {
+            for (int i = n + 1; i <= 2 * n; i++) {
+                if (prime[i]) {
                     count++;
                 }
             }
