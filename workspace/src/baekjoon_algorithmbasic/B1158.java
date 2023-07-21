bc5b824067fb8dcdd16084459395409a0676f6b7
hbeeni <hyebeen.dev@gmail.com>
2023-07-21 15:07:53 +0900

    백준 1158번: 요세푸스 문제

---
+++ workspace/src/baekjoon_algorithmbasic/B1158.java
@@ -0,0 +1,39 @@
+//https://dev-coco.tistory.com/167
+
+package baekjoon_algorithmbasic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B1158 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
+        }
+
+        sb.append("<");
+        while (q.size() != 1) {
+            //k - 1까지 처음에 있던 값을 뒤로 보냄
+            for (int i = 0; i < k - 1; i++) {
+                q.offer(q.poll());
+            }
+            sb.append(q.poll()).append(", ");
+        }
+        sb.append(q.poll()).append(">");
+
+        System.out.println(sb);
+    }
+}
