d8105f634c024c42c7f0c1deb829aabfe7014efb
hbeeni <hyebeen.dev@gmail.com>
2023-07-22 15:04:21 +0900

    프로그래머스 Lv. 1 - 숫자 문자열과 영단어

---
+++ workspace/src/programmers_lv1/숫자_문자열과_영단어.java
@@ -0,0 +1,24 @@
+//https://velog.io/@minjung0/Java-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%88%AB%EC%9E%90-%EB%AC%B8%EC%9E%90%EC%97%B4%EA%B3%BC-%EC%98%81%EB%8B%A8%EC%96%B4
+
+package programmers_lv1;
+
+public class 숫자_문자열과_영단어 {
+    public static int solution(String s) {
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (s.contains(arr[i])) {
+                s = s.replace(arr[i], Integer.toString(i));
+            }
+        }
+
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("one4seveneight"));
+        System.out.println(solution("23four5six7"));
+        System.out.println(solution("2three45sixseven"));
+        System.out.println(solution("123"));
+    }
+}
