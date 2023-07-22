2dafee43c259c71609b00b4350280e9ebde9df54
hbeeni <hyebeen.dev@gmail.com>
2023-07-22 17:22:06 +0900

    백준 1920번: 수 찾기

---
+++ 알고리즘 코딩테스트 (자바 편)/search/src/binarysearch/B1920.java
@@ -0,0 +1,57 @@
+package binarysearch;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int searchNum = Integer.parseInt(st.nextToken());
+            boolean find = false;
+            int s = 0;
+            int e = n - 1;
+
+            while (s <= e) {
+                int mid = (s + e) / 2;
+
+                if (searchNum == a[mid]) {
+                    find = true;
+                    break;
+                } else if (searchNum < a[mid]) {
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
+                }
+            }
+
+            if (find) {
+                bw.write("1");
+            } else {
+                bw.write("0");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+    }
+}
