357bb8b1ac89ef4b09762c9629dc5b78bd5113cf
Hyebeen <122146255+hbeeni@users.noreply.github.com>
2023-07-25 22:41:47 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,20 +1,23 @@
+//https://yongku.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%B9%B4%ED%8E%AB-%EC%9E%90%EB%B0%94JAVA
+
 import java.util.Arrays;
 
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int x = yellow, y = 1;
+        int[] answer = new int[2];
+        int sum = brown + yellow; //전체 타일 개수
 
-        for (;x >= 0 ; x--) {
-            if (yellow % x != 0) {
-                continue;
-            }
+        for (int i = 1; i <= sum; i++) {
+            int row = i;
+            int col = sum / row;
 
-            y = yellow / x;
-            if ((x + 2) * 2 + y * 2 == brown) {
+            if ((row - 2) * (col - 2) == yellow) {
+                answer[0] = col;
+                answer[1] = row;
                 break;
             }
         }
 
-        return new int[]{x + 2, y + 2};
+        return answer;
     }
 }
