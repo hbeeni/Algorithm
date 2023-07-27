6808ea247193b9db27fda6de560a7670fe1dd2a0
Hyebeen <122146255+hbeeni@users.noreply.github.com>
2023-07-27 22:29:04 +0900

    [Gold IV] Title: 수 묶기, Time: 128 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(a);
+
+        Queue<Integer> negative = new LinkedList<>();
+        Stack<Integer> positive = new Stack<>();
+        boolean zero = false;
+
+        for (int i : a) {
+            if (i < 0) {
+                negative.add(i);
+            } else if (i > 0) {
+                positive.push(i);
+            } else {
+                zero = true;
+            }
+        }
+
+        int answer = 0;
+
+        while (positive.size() >= 2 && positive.peek() != 1) {
+            int bigger = positive.pop();
+            int smaller = positive.pop();
+
+            if (smaller == 1) {
+                answer += bigger + smaller;
+                break;
+            }
+
+            answer += bigger * smaller;
+        }
+        while (!positive.isEmpty()) {
+            answer += positive.pop();
+        }
+
+        while (negative.size() >= 2) {
+            answer += negative.poll() * negative.poll();
+        }
+        if (!negative.isEmpty()) {
+            answer += zero ? 0 : negative.poll();
+        }
+
+        System.out.println(answer);
+    }
+}
