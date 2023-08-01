65220aa32bbec785f68a519462e3d53bb57a32fe
hbeeni <hyebeen.dev@gmail.com>
2023-08-02 08:36:48 +0900

    백준 11726번: 2 x n 타일링

---
+++ workspace/src/baekjoon/algorithm_basic_1/B11726.java
@@ -0,0 +1,24 @@
+//https://girawhale.tistory.com/33
+
+package baekjoon.algorithm_basic_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11726 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n + 2]; //n + 1 -> n = 1 일 때 a[2]에서 IndexOutOfBoundsException
+        a[1] = 1;
+        a[2] = 2;
+
+        //a[n] = a[n - 1] + a[n - 2]
+        for (int i = 3; i <= n; i++) {
+            a[i] = (a[i - 1] + a[i - 2]) % 10007;
+        }
+
+        System.out.println(a[n]);
+    }
+}
