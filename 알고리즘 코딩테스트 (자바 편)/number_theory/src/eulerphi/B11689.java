d3d4f8c1d929229c257fbf88b5b11e2d92b912de
hbeeni <hyebeen.dev@gmail.com>
2023-08-05 09:45:09 +0900

    백준 11689번: GCD(n, k) = 1

---
+++ 알고리즘 코딩테스트 (자바 편)/number_theory/src/eulerphi/B11689.java
@@ -0,0 +1,36 @@
+//못 품
+
+package eulerphi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B11689 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+
+        //n을 소인수분해 -> 소인수에 연산 수행
+        //소수 구하기: n의 제곱근까지
+        int max = (int) Math.sqrt(n);
+        boolean[] prime = new boolean[max + 1];
+        Arrays.fill(prime, true);
+        prime[0] = prime[1] = false;
+
+        for (int i = 2; i <= Math.sqrt(max); i++) {
+            for (int j = i * i; j <= max; j += i) {
+                prime[j] = false;
+            }
+        }
+
+        long answer = n;
+        //소수만 비교해서 연산 수행 n -= n / prime
+        for (int i = 2; i <= max; i++) {
+            if (prime[i] && n % i == 0) {
+                answer -= answer / i;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
---
+++ 알고리즘 코딩테스트 (자바 편)/number_theory/src/eulerphi/B11689_s.java
@@ -0,0 +1,26 @@
+package eulerphi;
+
+import java.util.Scanner;
+
+public class B11689_s {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long result = n;
+
+        for (long p = 2; p <= Math.sqrt(n); p++) {
+            if (n % p == 0) { //p가 소인면
+                result -= result / p;
+
+                while (n % p == 0) { //2^7 * 11이면 2^7을 없애고 11만 남김
+                    n /= p;
+                }
+            }
+        }
+
+        if (n > 1) { //아직 소인수 구성이 남았을 때 = 반복문에서 제곱근까지만 탐색 -> 1개의 소인수 누락된 케이스
+            result -= result / n;
+        }
+        System.out.println(result);
+    }
+}
