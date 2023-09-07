c2a4287236d949dcd8a9a8717e1f778f22cb0400
hbeeni <hyebeen.dev@gmail.com>
2023-09-07 19:09:40 +0900

    백준 9465번: 스티커

---
+++ workspace/src/baekjoon/algorithm_basic_1/dp1/exercise/B9465.java
@@ -0,0 +1,42 @@
+//https://velog.io/@yanghl98/%EB%B0%B1%EC%A4%80-9465-%EC%8A%A4%ED%8B%B0%EC%BB%A4-JAVA
+
+package baekjoon.algorithm_basic_1.dp1.exercise;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B9465 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            int[][] stickers = new int[2][n + 1];
+            int[][] dp = new int[2][n + 1];
+
+            for (int j = 0; j < 2; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int k = 1; k <= n; k++) {
+                    stickers[j][k] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            //column1은 자기 자신이 최대
+            dp[0][1] = stickers[0][1];
+            dp[1][1] = stickers[1][1];
+
+            //대각선, 대각선 왼쪽만 확인해서 큰 값 선택하면 됨
+            for (int j = 2; j <= n; j++) {
+                dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + stickers[0][j];
+                dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + stickers[1][j];
+            }
+
+            System.out.println(Math.max(dp[0][n], dp[1][n]));
+        }
+    }
+}
