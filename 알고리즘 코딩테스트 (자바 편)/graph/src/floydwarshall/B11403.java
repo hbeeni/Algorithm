ac5c5f57a7d1d40beadcc2f75fba2d8a1a067177
hbeeni <hyebeen.dev@gmail.com>
2023-09-13 13:17:43 +0900

    그래프 - 플로이드-워셜 (백준 11403번: 경로 찾기)

---
+++ 알고리즘 코딩테스트 (자바 편)/graph/src/floydwarshall/B11403.java
@@ -0,0 +1,62 @@
+package floydwarshall;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * bfs로 품
+ */
+public class B11403 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                int tmp = Integer.parseInt(st.nextToken());
+                if (tmp == 1) {
+                    list.get(i).add(j);
+                }
+            }
+        }
+
+        int[][] answer = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            Queue<Integer> queue = new LinkedList<>();
+            boolean[] visited = new boolean[n + 1];
+            queue.add(i);
+            visited[i] = true;
+
+            while (!queue.isEmpty()) {
+                int now = queue.poll();
+                ArrayList<Integer> nowList = list.get(now);
+
+                for (int j : nowList) {
+                    answer[i][j] = 1;
+
+                    if (!visited[j]) {
+                        visited[j] = true;
+                        queue.add(j);
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(answer[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
