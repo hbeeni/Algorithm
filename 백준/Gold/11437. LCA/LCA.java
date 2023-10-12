a5d7c1c3892265c4c805f54442317309a7e91f0d
Hyebeen <122146255+hbeeni@users.noreply.github.com>
2023-10-13 04:34:15 +0900

    [Gold III] Title: LCA, Time: 1508 ms, Memory: 43936 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.java
+++ 백준/Gold/11437. LCA/LCA.java
@@ -1,3 +1,5 @@
+//알고리즘 코딩 테스트 자바 편
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,12 +10,17 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static List<List<Integer>> nodes;
+    static int[] parent;
+    static int[] depth;
+    static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] parent = new int[n + 1];
 
-        List<List<Integer>> nodes = new ArrayList<>();
+        nodes = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
             nodes.add(new ArrayList<>());
         }
@@ -27,46 +34,64 @@ public class Main {
             nodes.get(b).add(a);
         }
 
-        int[] depth = new int[n + 1];
-        boolean[] visited = new boolean[n + 1];
+        //부모 노드, 깊이 배열 초기화
+        parent = new int[n + 1];
+        depth = new int[n + 1];
+        visited = new boolean[n + 1];
+        bfs();
+
+        int m = Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(executeLCA(a, b));
+        }
+    }
+
+    private static int executeLCA(int a, int b) {
+        //같은 깊이로 맞춰주기
+        if (depth[a] < depth[b]) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        while (depth[a] != depth[b]) {
+            a = parent[a];
+        }
+        //공통 조상 찾기
+        while (a != b) {
+            a = parent[a];
+            b = parent[b];
+        }
+        return a;
+    }
+
+    private static void bfs() {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(1);
         visited[1] = true;
-        depth[1] = 1;
+        int level = 1;
+        int nowSize = 1;
+        int count = 0;
 
         while (!queue.isEmpty()) {
             int now = queue.poll();
             for (int next : nodes.get(now)) {
                 if (!visited[next]) {
                     parent[next] = now;
-                    depth[next] = depth[now] + 1;
+                    depth[next] = level;
                     visited[next] = true;
                     queue.add(next);
                 }
             }
-        }
 
-        int m = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            //같은 깊이로 맞춰주기
-            while (depth[a] > depth[b]) {
-                a = parent[a];
+            count++;
+            if (count == nowSize) {
+                count = 0;
+                nowSize = queue.size();
+                level++;
             }
-            while (depth[a] < depth[b]) {
-                b = parent[b];
-            }
-            //공통 조상 찾기
-            while (a != b) {
-                a = parent[a];
-                b = parent[b];
-            }
-            sb.append(a).append("\n");
         }
-
-        System.out.println(sb);
     }
 }
