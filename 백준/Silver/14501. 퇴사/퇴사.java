95ea4b4aeb62d6daa373d14c590726c010dd7b13
Hyebeen <122146255+hbeeni@users.noreply.github.com>
2023-10-23 21:53:16 +0900

    [Silver III] Title: 퇴사, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] cases = new int[2][n + 1];
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            cases[0][i] = Integer.parseInt(st.nextToken());
+            cases[1][i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (cases[0][i] + i <= n + 1) {
+                dp[i] += cases[1][i];
+                for (int j = cases[0][i] + i; j <= n; j++) {
+                    dp[j] = Math.max(dp[j], dp[i]);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i : dp) {
+            max = Math.max(max, i);
+        }
+        System.out.println(max);
+    }
+}
