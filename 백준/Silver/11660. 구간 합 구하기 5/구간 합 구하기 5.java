101efce31b4304ae94cfdb30ad07a91a7bad35c2
hyebeen <122146255+hbeeni@users.noreply.github.com>
2024-05-27 11:30:59 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1720 ms, Memory: 128312 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -4,30 +4,37 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] a = new int[n + 1][n + 1];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int x = 1; x <= n; x++) {
-            st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken()); //표 크기
+		int m = Integer.parseInt(st.nextToken()); //합을 구해야 하는 횟수
 
-            for (int y = 1; y <= n; y++) {
-                a[x][y] = Integer.parseInt(st.nextToken()) + a[x][y - 1] + a[x - 1][y] - a[x - 1][y - 1];
-            }
-        }
+		int[][] a = new int[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+			for (int j = 1; j <= n; j++) {
+				a[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-            int sum = a[x2][y2] - a[x1 - 1][y2] - a[x2][y1 - 1] + a[x1 - 1][y1 - 1];
-            System.out.println(sum);
-        }
-    }
+		int[][] d = new int[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				d[i][j] = a[i][j] + d[i - 1][j] + d[i][j - 1] - d[i - 1][j - 1];
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+
+			System.out.println(d[x2][y2] - d[x1 - 1][y2] - d[x2][y1 - 1] + d[x1 - 1][y1 - 1]);
+		}
+	}
 }
